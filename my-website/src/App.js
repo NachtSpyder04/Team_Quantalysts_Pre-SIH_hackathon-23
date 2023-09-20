d3a5fd104920ff3829b863db2ec2601f006af2e1
saishock1504 <sskarole.1504@gmail.com>
2023-09-20 18:37:25 +0530

    Added Axios lib

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -5,9 +5,21 @@ import SignIn from "./sign_in";
 import SignUp from "./sign_up";
 import Parentcomponentmainpage from "./parent_component";
 import Addform from "./main page/addform";
+import { Axios } from "axios";
 
 
 function App() {
+    
+  const refreshList = () => {
+    Axios  //Axios to send and receive HTTP requests
+      .get("http://localhost:8000/api/tasks/")
+      .then(res => this.setState({ taskList: res.data }))
+      .catch(err => console.log(err));
+  };
+
+
+
+
   return (
     <div>    
       <Router>
